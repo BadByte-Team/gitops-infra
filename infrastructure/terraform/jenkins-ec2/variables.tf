3a24c0b740033855e49770a3d1d696080aa9d0a4
GN <144979549+GutsNet@users.noreply.github.com>
2026-04-16 09:33:19 -0600

    fix: cambiar tipo de instancia

--- infrastructure/terraform/jenkins-ec2/variables.tf
+++ infrastructure/terraform/jenkins-ec2/variables.tf
@@ -11,9 +11,9 @@ variable "ami_id" {
 }
 
 variable "instance_type" {
-  description = "Tipo de instancia — m7i-flex.large es Free Tier"
+  description = "Tipo de instancia — t3.micro es Free Tier"
   type        = string
-  default     = "m7i-flex.large"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
